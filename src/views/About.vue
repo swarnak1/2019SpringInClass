795209fc04978b87422ee312f2ac60a8df1afe64
Krishna Teja <teja.np18@gmail.com>
2019-03-11 14:04:47 -0400

    Building UI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row">
+    <div class="col">
+      <h1>This is {{name}}'s page. He is {{age}} years old </h1>
+      <input v-model="age" type="text" class="form-control" placeholder="Age" />
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" :style="{width: age + '%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+    data: ()=> ({
+      name: "Teja",
+      age: 21
+    })
+}
+</script>
+
+<style type="scss">
+</style>
