3d33bb7a98d2a21b9c9bb2c0cf4c7ef34f7cd9a6
Krishna Teja <teja.np18@gmail.com>
2019-03-14 18:35:36 -0400

    Nav bar

--- server/models/user.js
+++ server/models/user.js
@@ -10,7 +10,8 @@ const model = {
     },
     add(input, cb){
         conn.query( "INSERT INTO 2019Spring_Persons (FirstName,LastName,Birthday,Password,created_at) VALUES (?)",
-                    [[input.FirstName, input.LastName, input.Birthday, input.Password, new Date()]],
+                    [[input.
+                        FirstName, input.LastName, input.Birthday, input.Password, new Date()]],
                     (err, data) => {
                         cb(err, data);
                     }
